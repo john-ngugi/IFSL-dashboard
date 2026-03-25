bfbc49d50159b7fb907668b9cccefde76355908a
John Ngugi <johnngugi0407@gmail.com>
2026-03-26 00:01:00 +0300

    final final

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Navigation from "../ui/Navigation";
 import HeroSection from "../ui/Hero";
 import AboutSection from "../ui/AboutUs";
 import FeaturesSection from "../ui/FeatureSection";
-import StatsSection from "../ui/StatsData";
+// import StatsSection from "../ui/StatsData";
 import Footer from "../ui/Footer";
 import PartnersMarquee from "../ui/PartnersMarkee";
 import BeneficiariesSection from "../ui/BeneficiariesSection";
